b8d5293f8c37bad28b5dacfd3e7596d2be407e1a
JJJGuaca <jorgua87@gmail.com>
2019-11-04 23:38:12 -0500

    3ra subida

--- boton.js
+++ boton.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', menu);
 function menu(){
 	var contador=1;
-	$('.Lenadings').click(function(){
+	$('.tt').click(function(){
 		//$('nav').toggle();
 		if(contador==1){
-			$('nav').animate({
+			$('.nav-ul li').animate({
 				left:'0'
 			});
 			contador=0;
 		}else{
 			contador=1;
-			$('nav').animate({
+			$('.nav-ul li').animate({
 				left:'-100%'
 			});
 		}
